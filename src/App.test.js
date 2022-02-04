9b7431e084540979b5d1cd79e8e87361ec40acbb
Alice Hsing <alicehsing@gmail.com>
2022-02-03 16:17:44 -0800

    get test to pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Pass the arrays as props', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Pass the arrays as props/i);
   expect(linkElement).toBeInTheDocument();
 });
